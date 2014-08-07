0d7fd08fc2ad26f5c2e48a55335585963ae3affe
wanjunjun <wanjunjun@sina.com>
2014-08-07 09:44:21 +0800

    add thinkphp

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Home模块的Index控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+		echo "hello world in admin!";
+    }
+	
+	public function _before_index(){
+		echo 'before<br/>';
+	}
+	
+	public function _after_index(){
+		echo 'after<br/>';
+	} 
+	
+	public function hello($msg){
+		echo C("URL_MODEL");
+		echo "hello ".$msg." in admin"."<br>";
+		
+		$Test = new \My\Test();
+		$Test->sayHello();
+		echo "<br>".C("DEFAULT_AJAX_RETURN");
+	}
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'URL_HTML_SUFFIX' => 'shtml',
+	'URL_CASE_INSENSITIVE'  => true //url不区分大小写，针对linux系统
+);
