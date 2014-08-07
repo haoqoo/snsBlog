038e113d2c0e56c5cf611b0f77e63267273e15d6
haoqoo <qintao1203@163.com>
2014-08-07 16:56:01 +0800

    home page

--- Application/Admin/Model/UsersModel.class.php
+++ Application/Admin/Model/UsersModel.class.php
@@ -2,8 +2,5 @@
 namespace Admin\Model;
 use Think\Model;
 class UsersModel extends Model {
-	protected $_map = array(
-		'name' =>'username', // 把表单中name映射到数据表的username字段
- 		'mail' =>'email'// 把表单中的mail映射到数据表的email字段
- 	);
+	
 }
