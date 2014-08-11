08d0026aca2658b96b608170a2a0a3c3d14932ca
haoqoo <qintao1203@163.com>
2014-08-11 14:44:25 +0800

    category

--- Application/Home/Controller/CategoryController.class.php
+++ Application/Home/Controller/CategoryController.class.php
@@ -11,9 +11,19 @@ class CategoryController extends Controller {
     }
 
     public function show(){
-    	
-    	//$Categories = M("Categories");
-    	if(isset($_GET['id'])){
+    	$id = $_GET['id'];
+    	if(isset($id)){
+
+    		$Albums = M("Albums");    		
+    		$ablum_count = $Albums->where('category_id=%d',array($id))->count();
+
+    		$Posts = M("Posts");
+    		$post_count = $Posts->where('category_id=%d',array($id))->count();
+    		$catArray = C('CATEGORY');
+
+    		$this->assign('category',$catArray[$id]);
+    		$this->assign('post_count',$post_count);
+    		$this->assign('album_count',$ablum_count);
     		//$data = $Categories->find($_GET['id']);
     	}
     	$this->display(C('CATE_VIEW')."show");
--- Application/Home/Model/PostFavoritesModel.class.php
+++ Application/Home/Model/PostFavoritesModel.class.php
@@ -2,5 +2,5 @@
 namespace Home\Model;
 use Think\Model;
 class PostFavoritesModel extends Model {
-
+	
 }
